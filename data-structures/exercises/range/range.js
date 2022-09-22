5a9d5e9986507a80cf7299614d32afa8ea718b25
2Kelvin <kvin.mwangi254@gmail.com>
2022-09-22 17:05:20 +0300

    implemented negative step in range()

--- data-structures/exercises/range/range.js
+++ data-structures/exercises/range/range.js
@@ -1,9 +1,27 @@
-function range(firstNo, lastNo) {
+function range(firstNo, lastNo, step = (firstNo < lastNo) ? 1 : -1) {
     let anArray = [];
-    for (let i = firstNo; i <= lastNo; i++) {
-        anArray.push(i);
+    if (step > 0) {
+        for (let i = firstNo; i <= lastNo; i += step) {
+            anArray.push(i);
+        }
+    } else {
+        for (let i = firstNo; i >= lastNo; i += step) {
+            anArray.push(i);
+        }
     }
     return anArray;
 }
 
-console.log(range(11, 34));
+console.log(range(1, 10, 2));
+
+function sum(arrayParam) {
+    let total = 0;
+    for (let num of arrayParam) {
+        total = total + num;
+    }
+    return total;
+}
+
+console.log(sum(range(1, 10)));
+
+console.log(range(15, 8, -2)); 
